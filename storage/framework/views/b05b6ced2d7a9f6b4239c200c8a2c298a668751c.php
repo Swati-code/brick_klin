ea438c847a0f830f11307a7173eb30fbc7667e49
Swati-code <swatisharda29@gmail.com>
2021-07-20 23:25:38 +0530

    Add files via upload

---
+++ storage/framework/views/b05b6ced2d7a9f6b4239c200c8a2c298a668751c.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<h1 class="text-center mt-3">Company Details</h1>
+		<div class="row mt-3">
+			<div class="col-6 offset-3">
+				<form>
+					<div class="form-group">
+						<label for="">Company Name:</label>
+				        <input type="text" name="" class="form-control" placeholder="Enter company name">
+					</div>
+					<div class="form-group">
+						<label for="">Address:</label>
+				        <input type="text" name="" class="form-control" placeholder="Enter address">
+					</div>
+					<div class="form-group">
+						<label for="">Contact Number:</label>
+				        <input type="Number" name="" class="form-control" placeholder="Enter phone number">
+					</div>
+					<div class="row">
+						<div class="col">
+							<label class="mr-sm-2">PAN Number:</label>
+							<input type="text" class="form-control mb-2 mr-sm-2" id="" placeholder="Enter PAN number"name="">
+						</div>
+						 <div class="col">
+							<label class="mr-sm-2">VAT Number:</label>
+                             <input type="text" class="form-control mb-2 mr-sm-2" placeholder="Enter VAT number" name="">
+                         </div>
+					</div>
+					<div class="row">
+						<div class="col">
+							<label class="mr-sm-2">GST Number:</label>
+							<input type="text" class="form-control mb-2 mr-sm-2" id="" placeholder="Enter GST number"name="">
+						</div>
+						 <div class="col">
+							<label class="mr-sm-2">State Code:</label>
+                             <input type="text" class="form-control mb-2 mr-sm-2" placeholder="Enter state code" name="">
+                         </div>
+                         
+					<button type="submit" class="btn btn-success mt-3 w-100">Submit</button>
+				</form>
+			</div>
+		</div>
+
+	</div>
+
+</body>
+</html><?php /**PATH D:\xampp\htdocs\brick_klin\resources\views/company_details.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/b1a9f94ef046eadb525b8835f9d7030408a0b01e.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<h1 class="text-center mt-3">Product Details</h1>
+		<div class="row mt-3">
+			<div class="col-6 offset-3">
+				<form>
+					<div class="form-group">
+						<label for="">Product Name:</label>
+				        <input type="text" name="" class="form-control" placeholder="Enter product name">
+					</div>
+			        <div class="row">
+						<div class="col">
+							<label class="mr-sm-2">Product Code:</label>
+							<input type="text" class="form-control mb-2 mr-sm-2" id="" placeholder="Enter product code"name="">
+						</div>
+						<div class="col">
+							<label class="mr-sm-2">Select Category:</label>
+                             <select class="form-control"name="">
+							    <option value=""></option>
+						    </select>
+                        </div>
+					</div>
+					<div class="row">
+						<div class="col">
+							<label class="mr-sm-2">Tax Id:</label>
+							<input type="text" class="form-control mb-2 mr-sm-2" id="" placeholder="" name="">
+						</div>
+						<div class="col">
+							<label class="mr-sm-2">HSN Id:</label>
+                             <input type="text" class="form-control mb-2 mr-sm-2" id="" placeholder="" name="">
+                        </div>
+					</div>
+					<div class="row">
+						<div class="col">
+							<label for="">Price</label>
+							<input type="Number"class="form-control" id="" placeholder="">
+						</div>
+						<!-- <div class="col">
+							<label for="">
+								<textarea class="form-control"></textarea>
+							</label>
+						</div> -->
+					</div>
+					<div class="row">
+						<div class="col">
+							<label for="">Description:</label>
+							<textarea col="10" row="2" class="form-control"></textarea>
+						</div> 
+					</div>
+					<button type="submit" class="btn btn-success mt-3 w-100">Submit</button>
+				</form>
+			</div>
+		</div>
+
+	</div>
+
+</body>
+</html><?php /**PATH D:\xampp\htdocs\brick_klin\resources\views/products.blade.php ENDPATH**/ ?>
